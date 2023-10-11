f0378641ffb594290572f8384a931cccaa8f66b2
Alen Biju <60664814+dude0777@users.noreply.github.com>
2023-10-11 17:12:11 +0530

    initial commit

--- src/ExpenseList.js
+++ src/ExpenseList.js
@@ -11,7 +11,7 @@ function ExpenseList({ data }) {
 
         <div className={classes.cardcont}>
 
-            {data.map((item) => <ExpenseItem key={item.expenseName} name={item.expenseName} amount={item.amount} date={item.date} />)}
+            {data.map((item) => <ExpenseItem key={item.expenseName} name={item.expenseName} amount={item.amount} date={item.date} description={data.description} />)}
 
 
         </div>
